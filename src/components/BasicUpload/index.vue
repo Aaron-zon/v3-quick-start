603c720beba5bc60b6e28357729eff031bf8563f
dl_zhoujunyang <1640485761@qq.com>
2023-09-21 17:59:04 +0800

    m

---
+++ src/components/BasicUpload/index.vue
@@ -0,0 +1,162 @@
+<script setup>
+import { ref } from 'vue'
+import { genFileId } from 'element-plus'
+const props = defineProps([
+    'mode', // 上传控件模式
+    'btnName', // 按钮名字
+    'descName', // 描述
+
+    'fileInfos', // 预备上传文件信息
+    'url', // url
+    'multiple', // 允许多文件上传
+    'show-file-list', // 显示已上传文件
+    'autoUpload',  // 是否自动上传
+    'disabled', // 是否禁用上传
+    'limit', // 上传最大容许数
+    'drag', // 是否启动拖拽上传
+    'scoped-slot', // 属性改变默认的缩略图模板样式
+]);
+const emit = defineEmits([
+    'before-upload', // 上传文件之前的钩子
+    'change', // 文件状态改变时的钩子，添加文件、上传成功和上传失败时都会被调用
+]);
+
+const upload = ref(null);
+
+const handleChange = (file, files) => {
+    if (props.fileInfos) {
+        props.fileInfos.value = files;
+        props.fileInfos.ref = upload;
+    }
+    emit('change', {
+        file,
+        fileList: files
+    });
+}
+
+const handleOnlyFileExceed = (files) => {
+    upload.value.clearFiles();
+    const file = files[0];
+    file.uid = genFileId();
+    upload.value.handleStart(file);
+}
+
+/** 预览 */
+const handlePictureCardPreview = (file) => {
+
+}
+/** 下载 */
+const handleDownload = (file) => {
+
+}
+/** 删除 */
+const handleRemove = (file) => {
+
+}
+</script>
+
+<template>
+    <div class="basice-upload-container">
+        <!-- 1.单文件上传按钮 -->
+        <template v-if="props.mode == 0 || props.mode == undefined">
+            <el-upload 
+                ref="upload"
+                :action="props.url || '#'" 
+                :disabled="props.disabled || false"
+                :auto-upload="props.autoUpload || false"
+                :show-file-list="props.showFileList || true"
+                :limit="1"
+                :on-change="handleChange"
+                :on-exceed="handleOnlyFileExceed"
+            >
+                <el-button type="primary">{{ props.btnName || '点击上传' }}</el-button>
+                <template #tip>
+                    <div class="el-upload__tip" v-if="props.descName">
+                        {{ descName }}
+                    </div>
+                </template>
+            </el-upload>
+        </template>
+
+        <!-- 2.多文件上传按钮 -->
+        <template v-else-if="props.mode == 1">
+            <el-upload 
+                :action="props.url || '#'" 
+                :auto-upload="false"
+                :disabled="props.disabled || false"
+                multiple
+                :limit="props.limit || 1"
+            >
+                <el-button type="primary">{{ props.btnName || '点击上传' }}</el-button>
+                <template #tip>
+                    <div class="el-upload__tip" v-if="props.descName">
+                        {{ descName }}
+                    </div>
+                </template>
+            </el-upload>
+        </template>
+
+        <!-- 3.缩略图上传 -->
+        <template v-else-if="mode == 2">
+            <el-upload ref="upload" 
+                :action="props.url || '#'" 
+                list-type="picture-card" 
+                :auto-upload="false" 
+                :on-change="handleChange"
+            >
+                <el-icon><Plus /></el-icon>
+                <template #file="{ file }">
+                    <div>
+                        <img class="el-upload-list__item-thumbnail" :src="file.url" alt="" />
+                        <span class="el-upload-list__item-actions">
+                            <span
+                                class="el-upload-list__item-preview"
+                                @click="handlePictureCardPreview(file)"
+                            >
+                                <el-icon><zoom-in /></el-icon>
+                            </span>
+
+                            <span
+                                v-if="!disabled"
+                                class="el-upload-list__item-delete"
+                                @click="handleDownload(file)"
+                            >
+                                <el-icon><Download /></el-icon>
+                            </span>
+
+                            <span
+                                v-if="!disabled"
+                                class="el-upload-list__item-delete"
+                                @click="handleRemove(file)"
+                            >
+                                <el-icon><Delete /></el-icon>
+                            </span>
+                        </span>
+                    </div>
+                </template>
+            </el-upload>
+        </template>
+
+        <!-- 4.多类型文件可拖拽上传 -->
+        <template v-else-if="mode == 3">
+            <el-upload
+                drag
+                :action="url || '#'"
+                multiple
+            >
+                <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+                <div class="el-upload__text">
+                Drop file here or <em>click to upload</em>
+                </div>
+                <template #tip>
+                <div class="el-upload__tip">
+                    {{ descName || '文件小于500kb' }}
+                </div>
+                </template>
+            </el-upload>
+        </template>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
--- src/views/classicCase/upload/index.vue
+++ src/views/classicCase/upload/index.vue
@@ -1,15 +1,43 @@
 <script setup>
-import { ref } from 'vue'
-import Example from '@/components/example/index.vue'
+import { ref, reactive } from 'vue'
+import BasicUpload from '@/components/BasicUpload/index.vue'
+const aaa = () => {
+    console.log(fileInfos.value)
+}
+const bbb = () => {
+    fileInfos.ref.submit();
+}
+
+const fileInfos = reactive({});
+const url = 'http://localhost:9993/pino/testUpload';
+
 </script>
 
 <template>
     <div class="upload-container app-container">
+        <div class="upload-wrapper">
+            <!--  -->
+            <!-- <div>
+                <BasicUpload :fileInfos="fileInfos" :url="url" />
+            </div> -->
+
+            <div>
+                <BasicUpload :mode="2" :fileInfos="fileInfos" :url="url" />
+            </div>
+
+            <div>
+                <button @click="aaa">aaa</button>
+                <button @click="bbb">bbb</button>
+            </div>
+            
+
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
-.upload-container {
+.upload-wrapper {
     min-height: calc(var(--v3-appmain-height) - 50px);
+    background: #fff;
 }
 </style>
